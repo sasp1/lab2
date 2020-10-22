f5f45cd9beb00bf58a8b88b876e6916ece407dc3
Sebastian Arcos Specht <s164394@student.dtu.dk>
2020-10-22 10:14:07 +0200

    Initial commit

---
+++ src/main/java/Client.java
@@ -0,0 +1,12 @@
+import java.net.MalformedURLException;
+import java.rmi.Naming;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+
+public class Client {
+
+    public static void main(String[] args) throws RemoteException, NotBoundException, MalformedURLException {
+        HelloService service = (HelloService) Naming.lookup("rmi://localhost:5099/hello");
+        System.out.println("--- " + service.echo("hey server"));
+    }
+}
