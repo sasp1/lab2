e409432bb29ef631e6d8138de9f47e262c5a3306
Christian Hjelmslund <christianhjelmslund@gmail.com>
2020-11-16 12:40:06 +0100

    added four users with the different privileges and impleneted the RBAC

--- src/main/java/Client/Client.java
+++ src/main/java/Client/Client.java
@@ -55,6 +55,7 @@ public class Client {
                 break;
             } else {
                 chooseAction(input);
+                Thread.sleep(1000);
             }
         }
     }
--- src/main/java/Server/PrinterImpl.java
+++ src/main/java/Server/PrinterImpl.java
@@ -37,108 +37,127 @@ public class PrinterImpl extends UnicastRemoteObject implements IPrinter {
 
     @Override
     public String print(String filename, String printer, String session) {
-        if (currentSessionsToUser.get(session) == null) {
-            return "You are not authorized to this command";
-        } else {
+        User user = currentSessionsToUser.get(session);
+
+        if (!(user == null) && userPolicy.get(user.role).contains("print")) {
             String result = "Printing " + filename + " on printer " + printer+"\n";
-            System.out.println(result + "called by the user: " + currentSessionsToUser.get(session)+"\n");
+            System.out.println(result + "called by the user: " + user.name+"\n");
 
             return result;
+
+        } else {
+            return "You are not authorized to this command";
         }
     }
 
     @Override
     public String queue(String printer,  String session) {
-        if (currentSessionsToUser.get(session) == null) {
-            return "You are not authorized to this command";
-        } else {
+        User user = currentSessionsToUser.get(session);
+
+        if (!(user == null) && userPolicy.get(user.role).contains("queue")) {
             String result = "JOB\tFILENAME\n323\t02239_lab1.pdf\n324\t02233_lab1.pdf" +
                     "\n325\tinternet-cat.png\n326\tmy-credit-card-details.pdf\n327\tdata-security.pdf\n";
-            System.out.println(result + "called by: " + currentSessionsToUser.get(session)+"\n");
+            System.out.println(result + "called by: " + user.name+"\n");
             return result;
+
+        } else {
+            return "You are not authorized to this command";
         }
     }
 
     @Override
     public String topQueue(String printer, int job, String session) {
-        if (currentSessionsToUser.get(session) == null) {
-            return "You are not authorized to this command";
-        } else {
+        User user = currentSessionsToUser.get(session);
+
+        if (!(user == null) && userPolicy.get(user.role).contains("top queue")) {
             String result = "Moving job " + job + " to the top of the queue on printer " + printer + "...\nDone!\n";
-            System.out.println(result + "called by: " + currentSessionsToUser.get(session)+"\n");
+            System.out.println(result + "called by: " + user.name+"\n");
             return result;
+        } else {
+            return "You are not authorized to this command";
         }
     }
 
     @Override
     public String start(String session) {
-        if (currentSessionsToUser.get(session) == null) {
-            return "You are not authorized to this command";
-        } else {
+        User user = currentSessionsToUser.get(session);
+
+        if (!(user == null) && userPolicy.get(user.role).contains("start")) {
             String result = "Starting print server...\n";
-            System.out.println(result + "called by: " + currentSessionsToUser.get(session)+"\n");
+            System.out.println(result + "called by: " + user.name+"\n");
             return result;
+        } else {
+            return "You are not authorized to this command";
         }
     }
 
     @Override
     public String stop(String session) {
-        if (currentSessionsToUser.get(session) == null) {
-            return "You are not authorized to this command";
-        } else {
+        User user = currentSessionsToUser.get(session);
+
+        if (!(user == null) && userPolicy.get(user.role).contains("stop")) {
             String result = "Stopping print server...\n";
-            System.out.println(result + "called by: " + currentSessionsToUser.get(session)+"\n");
+            System.out.println(result + "called by: " + user.name+"\n");
             return result;
+        } else {
+            return "You are not authorized to this command";
         }
     }
 
     @Override
     public String restart(String session) {
-        if (currentSessionsToUser.get(session) == null) {
-            return "You are not authorized to this command";
-        } else {
+        User user = currentSessionsToUser.get(session);
+
+        if (!(user == null) && userPolicy.get(user.role).contains("restart")) {
             String result = "Restart initiated...\nPrint queue cleared...\nRebooting...\n";
-            System.out.println(result + "called by: " + currentSessionsToUser.get(session)+"\n");
+            System.out.println(result + "called by: " + user.name+"\n");
             return result;
+        } else {
+            return "You are not authorized to this command";
         }
     }
 
     @Override
     public String status(String printer, String session) {
-        if (currentSessionsToUser.get(session) == null) {
-            return "You are not authorized to this command";
-        } else {
+        User user = currentSessionsToUser.get(session);
+
+        if (!(user == null) && userPolicy.get(user.role).contains("status")) {
             String result = "Status of printer " + printer + ": Online and available.\n";
-            System.out.println(result + "called by: " + currentSessionsToUser.get(session)+"\n");
+            System.out.println(result + "called by: " + user.name+"\n");
             return result;
+        } else {
+            return "You are not authorized to this command";
         }
     }
 
     @Override
     public String readConfig(String parameter, String session) {
-        if (currentSessionsToUser.get(session) == null) {
-            return "You are not authorized to this command";
-        } else {
+        User user = currentSessionsToUser.get(session);
+
+        if (!(user == null) && userPolicy.get(user.role).contains("read configuration")) {
             String result = parameter + " = " + "TRUE\n";
-            System.out.println(result + "called by: " + currentSessionsToUser.get(session)+"\n");
+            System.out.println(result + "called by: " + user.name+"\n");
             return result;
+        } else {
+            return "You are not authorized to this command";
         }
     }
 
     @Override
     public String setConfig(String parameter, String value, String session) {
-        if (currentSessionsToUser.get(session) == null) {
-            return "You are not authorized to this command";
-        } else {
+        User user = currentSessionsToUser.get(session);
+
+        if (!(user == null) && userPolicy.get(user.role).contains("set configuration")) {
             String result = "Setting parameter " + parameter + " equal to " + value + "...\nDone!\n";
-            System.out.println(result + "called by: " + currentSessionsToUser.get(session)+"\n");
+            System.out.println(result + "called by: " + user.name+"\n");
             return result;
+        } else {
+            return "You are not authorized to this command";
         }
     }
 
     @Override
     public String login(String inputUsername, String inputPassword) {
-
         try {
             File file = new File("src/main/java/Server/password.txt");
             FileReader fr = new FileReader(file);
